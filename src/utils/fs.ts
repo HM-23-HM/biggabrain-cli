e288a4805db9011067335a8c406b6a012cd464b9
Hodeem Miller <miller.hodeem@gmail.com>
2024-12-26 13:57:10 -0500

    Made getPdfFilenames more generic

--- src/utils/fs.ts
+++ src/utils/fs.ts
@@ -1,11 +1,11 @@
 import * as path from 'path';
 import * as fs from 'fs';
 
-export function getPdfFilenames() {
-    const inboundFolder = path.join(__dirname, '../../inbound');
-    if (!fs.existsSync(inboundFolder)) {
-        throw new Error('Inbound folder does not exist.');
+export function getPdfFilenames(folder: string = 'inbound') {
+    const folderPath = path.join(__dirname, `../../${folder}`);
+    if (!fs.existsSync(folderPath)) {
+        throw new Error(`${folder} folder does not exist.`);
     }
 
-    return fs.readdirSync(inboundFolder).filter(file => path.extname(file).toLowerCase() === '.pdf');
+    return fs.readdirSync(folderPath).filter(file => path.extname(file).toLowerCase() === '.pdf');
 }
